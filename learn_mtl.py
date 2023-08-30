49af91bbd7cbbe1c4110f885e5aa726eab41d41d
Rajarshi Roy <rajarshi008@gmail.com>
2023-08-30 17:15:43 +0200

    change before running

--- learn_mtl.py
+++ learn_mtl.py
@@ -292,7 +292,7 @@ def run_test(file_name, timeout=5400, fr_bound=3):
 		writer.writerow(info_dict)
 
 
-run_test('dummy.signal', 900, 2)
+#run_test('dummy.signal', 900, 2)
 
 '''
 #return #the predicates
