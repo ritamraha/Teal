ecd1e3879a8c4dc7b794813df4399f33fe19401c
Rajarshi Roy <rajarshi008@gmail.com>
2023-09-15 11:28:31 +0200

    grid check

--- grid-batch.sh
+++ grid-batch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH -w spyder2[0-3]        # Use spyder
+#SBATCH -w spyder20        # Use spyder
 #SBATCH -c 1                   # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
-#SBATCH -t 0-02:00              # Maximum run-time in D-HH:MM
-#SBATCH --mem=100G               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -t 0-00:10              # Maximum run-time in D-HH:MM
+#SBATCH --mem=10G               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o hostname_%A_%a.out   # File to which STDOUT will be written
 #SBATCH -e hostname_%A_%a.err   # File to which STDERR will be written
 #SBATCH --array=1-12            # Number of tasks in the array
@@ -17,7 +17,7 @@ signal_files=($(find "$folder" -type f -name "*.signal"))
 
 
 # Get the current signal_file for this task
-current_signal_file=${signal_files[{$SLURM_ARRAY_TASK_ID - 1}%4]}
-fr_bound={$SLURM_ARRAY_TASK_ID}%3
+current_signal_file=${signal_files[$((SLURM_ARRAY_TASK_ID - 1))/4]}
+fr_bound=$((SLURM_ARRAY_TASK_ID % 3))
 
 python learn_mtl.py -i "$current_signal_file" -f 1
